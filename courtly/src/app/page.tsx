5c3f7ecabf7c0e0e1cdc0b1d3b13d7f153fab259
John Patrick Marcellana <jptmarcellana@gmail.com>
2024-10-12 16:07:31 +0800

    initial commit

---
+++ courtly/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-5xl">Courtly Web App</h1>
+    </div>
+      
+  );
+}
